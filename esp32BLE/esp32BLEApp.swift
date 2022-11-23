35da5b9142141da7f801673ece2f6278ad1d1aa2
Oleksii Mykhalchuk <ale.m100888@gmail.com>
2022-11-23 16:08:59 +0100

    Initial Commit

---
+++ esp32BLE/esp32BLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  esp32BLEApp.swift
+//  esp32BLE
+//
+//  Created by Oleksii Mykhalchuk on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct esp32BLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
